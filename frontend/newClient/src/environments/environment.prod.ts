ba778969a0c91342189c2564ff46dc8b82997a88
citypayorg <citypay.org@gmain.com>
2020-12-17 15:28:32 +0900

    비번

--- frontend/newClient/src/environments/environment.prod.ts
+++ frontend/newClient/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   version: '0.0.3-ALPHA',
-  apiServer: 'http://web-poker.tandilserver.com:8083/',
+  // apiServer: 'http://web-poker.tandilserver.com:8083/',
+  apiServer: 'http://api.poker.citypay.org/',
   refreshProfileTime: 2500,
   stompDebug: false
 };
